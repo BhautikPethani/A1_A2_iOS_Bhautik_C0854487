4b11b3d27cc3b198da17a939cc106e3a31985f26
BhautikPethani <c0854487@mylambton.ca>
2022-05-24 17:38:42 -0400

    Initial Commit

---
+++ A1_A2_iOS_Bhautik_C0854487/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A1_A2_iOS_Bhautik_C0854487
+//
+//  Created by Bhautik Pethani on 2022-05-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
